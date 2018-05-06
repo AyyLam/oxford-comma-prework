224168abb1b2f004a024f198b9e78e29aeadb4e7
Anthony Lam <al3101@nyu.edu>
2018-05-06 17:25:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,16 @@ def oxford_comma(array)
     return array 
   elsif array.size == 2 
     return array[0] + array[1] 
-
+  else 
+    counter = 0
+    sentence = ""
+    array.each do |element| 
+      if counter == array.size - 1 
+        sentence += "and #{array[counter]}" 
+      else 
+        sentence += "#{array[counter]}, "
+      end 
+      return sentence 
+    end
+  end
 end
